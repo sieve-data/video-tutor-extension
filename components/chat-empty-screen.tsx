11079ddfd629b02b6264fc7b15db6a8c766bd3e2
JetJadeja <jetjadeja@gmail.com>
2025-06-10 10:31:58 -0700

    change name to breakdown

--- components/chat-empty-screen.tsx
+++ components/chat-empty-screen.tsx
@@ -1,38 +1,41 @@
-import { Button } from "@/components/ui/button"
-import { IconSparkles } from "@/components/ui/icons"
-import { useChat } from "@/contexts/chat-context"
-import { useExtension } from "@/contexts/extension-context"
-import { cn } from "@/lib/utils"
-import { ArrowRightIcon } from "@radix-ui/react-icons"
-import React from "react"
+import { Button } from "@/components/ui/button";
+import { IconSparkles } from "@/components/ui/icons";
+import { useChat } from "@/contexts/chat-context";
+import { useExtension } from "@/contexts/extension-context";
+import { cn } from "@/lib/utils";
+import { ArrowRightIcon } from "@radix-ui/react-icons";
+import React from "react";
 
-import { usePort } from "@plasmohq/messaging/hook"
+import { usePort } from "@plasmohq/messaging/hook";
 
 const exampleMessages = [
   {
     heading: "What is the video about?",
-    message: "Can you tell me about the video?"
+    message: "Can you tell me about the video?",
   },
   {
     heading: "What are the key points?",
-    message: "What are the key points of the video?"
+    message: "What are the key points of the video?",
   },
   {
     heading: "What are the main takeaways?",
-    message: "What are the main takeaways of the video?"
+    message: "What are the main takeaways of the video?",
   },
   {
     heading: "What are the main topics?",
-    message: "What are the main topics discussed in the video?"
-  }
-]
+    message: "What are the main topics discussed in the video?",
+  },
+];
 
 interface EmptyScreenProps {
-  className?: string
-  setPromptInput: (value: any) => void
+  className?: string;
+  setPromptInput: (value: any) => void;
 }
 
-export default function EmptyScreen({ className, setPromptInput }: EmptyScreenProps) {
+export default function EmptyScreen({
+  className,
+  setPromptInput,
+}: EmptyScreenProps) {
   // const port = usePort("suggestions")
 
   // const {
@@ -89,14 +92,13 @@ export default function EmptyScreen({ className, setPromptInput }: EmptyScreenPr
     <div className={cn("mx-auto px-8", className)}>
       <div className="rounded-md bg-background p-8 w-full justify-center flex flex-col items-center">
         <span className="text-2xl flex items-center mb-8">
-          Youtube
           <IconSparkles className="inline mr-0 ml-0.5 w-4 sm:w-5 mb-1" />
-          AI
+          Breakdown
         </span>
         <p className="mb-4 leading-normal text-muted-foreground text-center opacity-70">
-          A conversational AI extension for YouTube videos that allows users to interact
-          directly with content. Ask specific questions or seek detailed information about
-          any part of the video.
+          Breakdown is a tool that provides real-time concept explanations and
+          provides users with a chat interface to ask questions about the
+          content.
         </p>
 
         <p className="leading-normal text-muted-foreground mb-8 opacity-70">
@@ -113,7 +115,8 @@ export default function EmptyScreen({ className, setPromptInput }: EmptyScreenPr
               key={index}
               variant="outline"
               onClick={() => setPromptInput(message.message)}
-              className="h-auto w-full  justify-start dark:bg-transparent border-[0.5px] p-3 opacity-80">
+              className="h-auto w-full  justify-start dark:bg-transparent border-[0.5px] p-3 opacity-80"
+            >
               <ArrowRightIcon className="mr-2 text-muted-foreground" />
               {message.heading}
             </Button>
@@ -121,14 +124,14 @@ export default function EmptyScreen({ className, setPromptInput }: EmptyScreenPr
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 interface SuggestionItemProps {
-  message: string
-  index: number
-  idx: number
-  setPromptInput: (message: string) => void
+  message: string;
+  index: number;
+  idx: number;
+  setPromptInput: (message: string) => void;
 }
 
 const SuggestionItem: React.FC<SuggestionItemProps> = React.memo(
@@ -138,9 +141,10 @@ const SuggestionItem: React.FC<SuggestionItemProps> = React.memo(
         key={`${index}-${idx}`}
         variant="outline"
         onClick={() => setPromptInput(message)}
-        className="h-auto w-full justify-start dark:bg-transparent border-[0.5px] p-3 opacity-80 text-wrap">
+        className="h-auto w-full justify-start dark:bg-transparent border-[0.5px] p-3 opacity-80 text-wrap"
+      >
         {message}
       </Button>
-    )
+    );
   }
-)
+);
